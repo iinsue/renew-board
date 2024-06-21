aafcc08e1c514adf72bba6b1358daa64ed0e4bcd
insu <hinsu93@gmail.com>
2024-06-21 20:17:18 +0900

    tailwind prettier 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
       <div>Home</div>
     </main>
   );
