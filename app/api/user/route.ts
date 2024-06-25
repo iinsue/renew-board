d2a59787963b36e60d54b2e24644101665a7ec41
insu <hinsu93@gmail.com>
2024-06-25 19:47:58 +0900

    Update route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,10 +1,15 @@
+import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     console.log("server", body);
-    return new NextResponse("Check User", { status: 200 });
+
+    const user = await prismadb.user.create({
+      data: body,
+    });
+    return NextResponse.json(user);
   } catch (error) {
     console.log("[USER_POST]", error);
     return new NextResponse("Internal error", { status: 500 });
