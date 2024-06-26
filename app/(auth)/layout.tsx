7e2b0e0096dc5648bfdad512593e4fa48ee99b1e
insu <hinsu93@gmail.com>
2024-06-26 19:01:57 +0900

    add login signup page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  children: React.ReactNode;
+};
+
+const AuthLayout = ({ children }: Props) => {
+  return (
+    <>
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default AuthLayout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
-import { TestForm } from "@/components/input-form";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center bg-sky-700 px-4 py-2 sm:px-8 sm:py-3">
-      <div className="w-[300px]">
-        <TestForm />
+      <div className="w-[300px] space-y-4 rounded-md bg-white p-4">
+        <Button className="w-full" asChild>
+          <Link href="/sign-up">회원가입</Link>
+        </Button>
+        <Button className="w-full" asChild>
+          <Link href="/sign-in">로그인</Link>
+        </Button>
       </div>
     </main>
   );
