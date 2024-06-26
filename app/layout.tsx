b4d3e840e5b0f5ab7abae37280fdf24efb063cf1
insu <hinsu93@gmail.com>
2024-06-26 20:01:05 +0900

    add sonner toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <Toaster position="top-center" duration={1000} />
       <body className={inter.className}>{children}</body>
     </html>
   );
