bbb852c90a939b7795d9fb26adae3ae49a25d61b
iinsue <hinsu93@naver.com>
2024-07-01 23:10:55 +0900

    session callback 설정

--- auth.ts
+++ auth.ts
@@ -6,6 +6,17 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    async jwt({ token, user, profile }) {
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   pages: {
