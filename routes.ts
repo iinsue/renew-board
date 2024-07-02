4447e03f00114e82eaaba266ef4e64d8bb06445d
iinsue <hinsu93@naver.com>
2024-07-02 23:06:00 +0900

    유저 세션 반환 함수 추가

--- routes.ts
+++ routes.ts
@@ -1,26 +1,23 @@
 /**
- * An array of routes that are accessible to the public
- * These routes do not require authentication
+ * 로그인 없이 접근 가능한 경로
  * @type {string[]}
  */
 export const publicRoutes = ["/"];
 
 /**
- * An array of routes that are used for authentication
- * These routes will redirect logged in users to /
+ * 로그인된 사용자는 접근할 수 없는 경로
  * @type {string[]}
  */
 export const authRoutes = ["/sign-in", "/sign-up"];
 
 /**
- * The prefix for API authentication routes
- * Routes that start with this prefix are used for API authentication purposes
+ * 인증 api 경로
  * @type {string}
  */
 export const apiAuthPrefix = "/api/auth";
 
 /**
- * The default redirect path after logging in
+ * 로그인 성공 시 이동되는 페이지
  * @type {string}
  */
 export const DEFAULT_LOGIN_REDIRECT = "/main";
