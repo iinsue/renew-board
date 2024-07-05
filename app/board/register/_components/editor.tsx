cb9921347a8fe6544b74c42f330d24861d314e4e
insu <hinsu93@gmail.com>
2024-07-05 16:21:25 +0900

    게시글 등록 시 redirect

--- app/board/register/_components/editor.tsx
+++ app/board/register/_components/editor.tsx
@@ -5,8 +5,11 @@ import { Button } from "@/components/ui/button";
 import { EditorState } from "lexical";
 import { useState } from "react";
 import axios from "axios";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const TextEditor = () => {
+  const router = useRouter();
   const [editorState, setEditorState] = useState<string | null>(null);
   const onChange = (editorState: EditorState) => {
     setEditorState(JSON.stringify(editorState.toJSON()));
@@ -15,7 +18,10 @@ export const TextEditor = () => {
     const response = await axios.post("/api/post", {
       content: editorState,
     });
-    console.log("response", response);
+    if (response.status === 200) {
+      toast.success("게시글이 등록되었습니다.", { id: "board" });
+      router.replace("/board");
+    }
   };
 
   return (
