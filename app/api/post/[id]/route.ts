2da004550b72cfc2c3a33ef3fd6b62751f761ee5
insu <hinsu93@gmail.com>
2024-07-08 20:28:07 +0900

    게시글 삭제 API 추가

--- app/api/post/[id]/route.ts
+++ app/api/post/[id]/route.ts
@@ -2,12 +2,13 @@ import { userSession } from "@/lib/auth";
 import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } },
-) {
-  const { id } = params;
+type Props = {
+  params: { id: string };
+};
+
+export async function GET(request: Request, { params }: Props) {
   try {
+    const { id } = params;
     const user = await userSession();
     if (!user) {
       return new NextResponse("로그인 후 이용해주세요", { status: 401 });
@@ -18,6 +19,20 @@ export async function GET(
     });
 
     return NextResponse.json(post, { status: 200 });
+  } catch (error) {
+    console.log("[게시글 상세 조회 오류]", error);
+    return new NextResponse("서버 오류", { status: 500 });
+  }
+}
+
+export async function DELETE(request: Request, { params }: Props) {
+  try {
+    const { id } = params;
+    await db.post.delete({ where: { id } });
+    return NextResponse.json(
+      { message: "게시글이 삭제되었습니다." },
+      { status: 200 },
+    );
   } catch (error) {
     console.log("[게시글 상세 조회 오류]", error);
     return new NextResponse("서버 오류", { status: 500 });
